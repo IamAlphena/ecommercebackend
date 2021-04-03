d5075f41965116a1ed9521d9534eba71bc9d72db
Alphena <alphena@gmail.com>
2021-04-02 22:52:40 -0700

    colums and belongsto connections made

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //foreign key 
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //foreign key
+    }
   },
   {
     sequelize,
